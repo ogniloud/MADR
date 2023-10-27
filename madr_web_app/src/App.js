337e007997c62da26a30631fc7056bdf74ceb985
Rafik <69720999+rafi2002@users.noreply.github.com>
2023-10-27 23:49:13 +0700

    sign-up page

---
+++ madr_web_app/src/App.js
@@ -0,0 +1,15 @@
+import './Style-component/App.css'
+import './Style-component/Sign-up-page.css'
+import Form from './components/Form';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Form>
+        </Form>
+    </div>
+  );
+}
+
+export default App;
