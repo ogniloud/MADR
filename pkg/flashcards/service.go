0ea4b50deedcb663a81db58365d4d49f90d5f8e0
pelageech <va.oleg2015@yandex.ru>
2023-10-27 13:16:50 +0700

    implemented service

--- pkg/flashcards/service.go
+++ pkg/flashcards/service.go
@@ -1,25 +1,90 @@
 package flashcards
 
-import "github.com/ogniloud/madr/pkg/flashcards/storage"
+import (
+	"github.com/ogniloud/madr/pkg/flashcards/cache"
+	"github.com/ogniloud/madr/pkg/flashcards/storage"
+)
 
-type DeckConfig struct {
-	Name string `json:"name"`
-}
+var (
+// ErrDatabaseAccess = fmt.Errorf("error when accessing db")
+)
 
-// Service must be
-type Service interface {
-	LoadDecks() ([]storage.DeckId, error)
+// IService must be
+type IService interface {
+	LoadDecks(id storage.UserId) ([]storage.DeckConfig, error)
 	GetFlashcards(id storage.DeckId) ([]storage.Flashcard, error)
 
 	/* decks can be changed only if they are not public */
 	AppendFlashcard(id storage.DeckId, flashcard storage.Flashcard) error
 	DeleteFlashcard(deckId storage.DeckId, flashcardId storage.FlashcardId) error
 
-	CreateNewDeck(cfg DeckConfig, flashcards []storage.Flashcard) error
+	CreateNewDeck(userId storage.UserId, cfg storage.DeckConfig, flashcards []storage.Flashcard) error
 	DeleteDeck(id storage.DeckId) error
 }
 
-// Handler with methods http.HandlerFunc
-type Handler struct {
-	s *storage.Storage
+type Service struct {
+	s storage.Storage
+
+	// temporary
+	c cache.Cache
+}
+
+func (s Service) LoadDecks(id storage.UserId) (storage.Decks, error) {
+	if decksAny, ok := s.c.Load(id); ok {
+		decks := decksAny.(storage.Decks)
+		return decks, nil
+	}
+
+	decks, err := s.s.GetDecksByUserId(id)
+	if err != nil {
+		return nil, err
+	}
+	go s.c.Store(id, decks)
+
+	return decks, nil
+}
+
+func (s Service) GetFlashcards(id storage.DeckId) ([]storage.Flashcard, error) {
+	return s.s.GetFlashcardsByDeckId(id)
+}
+
+func (s Service) AppendFlashcard(id storage.DeckId, flashcard storage.Flashcard) error {
+	return s.s.PutFlashcard(id, flashcard)
+}
+
+func (s Service) DeleteFlashcard(deckId storage.DeckId, flashcardId storage.FlashcardId) error {
+	return s.s.DeleteFlashcardFromDeck(deckId, flashcardId)
+}
+
+func (s Service) CreateNewDeck(userId storage.UserId, cfg storage.DeckConfig, flashcards []storage.Flashcard) error {
+	decks, err := s.LoadDecks(userId)
+	if err != nil {
+		return err
+	}
+
+	if err := s.s.PutNewDeck(cfg); err != nil {
+		return err
+	}
+
+	if err := s.s.PutAllFlashcards(cfg.Id, flashcards); err != nil {
+		return err
+	}
+
+	decks[cfg.Id] = cfg // maybe critical section!
+
+	return nil
+}
+
+func (s Service) DeleteDeck(userId storage.UserId, deckId storage.DeckId) error {
+	decks, err := s.LoadDecks(userId)
+	if err != nil {
+		return err
+	}
+
+	if err := s.s.DeleteDeck(deckId); err != nil {
+		return err
+	}
+
+	delete(decks, deckId)
+	return nil
 }
