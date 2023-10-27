79b02eeb456b67ac22dea68e7b6920dd1b9f86bb
pelageech <va.oleg2015@yandex.ru>
2023-10-27 13:16:37 +0700

    added Decks type containing deck configs by id

--- pkg/flashcards/storage/storage.go
+++ pkg/flashcards/storage/storage.go
@@ -9,37 +9,26 @@ type UserId = int
 type FlashcardId = int
 
 type DeckConfig struct {
-	Id      DeckId
-	Name    string
-	Private bool
-}
-
-type DeckConfigStorage interface {
-	Get(id DeckId) (DeckConfig, error)
-	Put(config DeckConfig) error
-	Delete(id DeckId) error
+	Id   DeckId `json:"id"`
+	Name string `json:"name"`
 }
 
 type Word = string
+type BacksideType int
 
 // Backside is an abstract type for representation an answer
 // of describing the Word. That can be just a string (translation, definition)
 // or image, or sound - everything depends on Answer method.
-type Backside interface {
-	Answer() any
+type Backside struct {
+	Type  BacksideType `json:"type"`
+	Value string       `json:"value"`
 }
 
 type Flashcard struct {
-	Id     FlashcardId
-	W      Word
-	B      Backside
-	DeckId DeckId
-}
-
-type FlashcardStorage interface {
-	Get(id FlashcardId) (Flashcard, error)
-	Put(flashcard Flashcard) error
-	Delete(id FlashcardId) error
+	Id     FlashcardId `json:"id"`
+	W      Word        `json:"word"`
+	B      Backside    `json:"backside"`
+	DeckId DeckId      `json:"deck_id"`
 }
 
 type LeitnerId = int
@@ -51,25 +40,20 @@ type Level = int
 type CoolDown struct {
 
 	// the current simulation time.
-	state time.Time
-
-	// the function should return the time
-	// that must pass from the moment this function is called
-	// for the flashcard to become accessible again.
-	f func(l Level) time.Duration
+	State time.Time `json:"state"`
 }
 
 func NewCoolDown(startState time.Time, f func(l Level) time.Duration) CoolDown {
-	return CoolDown{state: startState, f: f}
+	return CoolDown{State: startState}
 }
 
 func (cd CoolDown) String() string {
-	return cd.String()
+	return cd.State.String()
 }
 
 // NextState updates the state of CoolDown relatively time.Now
-func (cd CoolDown) NextState(l Level) {
-	cd.state = time.Now().Add(cd.f(l))
+func (cd CoolDown) NextState(l Level, f func(l Level) time.Duration) {
+	cd.State = time.Now().Add(f(l))
 }
 
 // IsPassedNow returns true if state of CoolDown is not less than time.Now
@@ -79,25 +63,25 @@ func (cd CoolDown) IsPassedNow() bool {
 
 // IsPassed returns true if state of CoolDown is not less than t
 func (cd CoolDown) IsPassed(t time.Time) bool {
-	return cd.state.Compare(t) != -1
+	return cd.State.Compare(t) != -1
 }
 
 type UserLeitner struct {
-	Id          LeitnerId
-	UserId      UserId
-	FlashcardId FlashcardId
-	Level       Level
-	CoolDown    CoolDown
+	Id          LeitnerId   `json:"id"`
+	UserId      UserId      `json:"user_id"`
+	FlashcardId FlashcardId `json:"card_id"`
+	Level       Level       `json:"level"`
+	CoolDown    CoolDown    `json:"cooldown"`
 }
 
-type LeitnerStorage interface {
-	Get(id LeitnerId) (UserLeitner, error)
-	Put(leitner UserLeitner) error
-	Delete(id LeitnerId) error
-}
+type Decks map[DeckId]DeckConfig
 
-type Storage struct {
-	Dcs DeckConfigStorage
-	Fs  FlashcardStorage
-	Ls  LeitnerStorage
+type Storage interface {
+	GetDecksByUserId(id UserId) (Decks, error)
+	GetFlashcardsByDeckId(id DeckId) ([]Flashcard, error)
+	PutFlashcard(id DeckId, card Flashcard) error
+	PutAllFlashcards(id DeckId, cards []Flashcard) error
+	DeleteFlashcardFromDeck(id DeckId, cardId FlashcardId) error
+	PutNewDeck(config DeckConfig) error
+	DeleteDeck(id DeckId) error
 }
