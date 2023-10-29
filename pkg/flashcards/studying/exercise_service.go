74247906dd5c57255a3a9294a229a2d5e67d4858
pelageech <va.oleg2015@yandex.ru>
2023-10-29 19:21:30 +0700

    struct of exercises

--- pkg/flashcards/studying/exercise_service.go
+++ pkg/flashcards/studying/exercise_service.go
@@ -1,5 +1,7 @@
 package studying
 
+import "github.com/ogniloud/madr/pkg/flashcards"
+
 type Matching struct{}
 
 type Sentence struct{}
@@ -7,4 +9,22 @@ type Sentence struct{}
 type IExerciseService interface {
 	MakeMatching() Matching
 	MakeSentence() Sentence
+}
+
+type ExerciseService struct {
+	s *flashcards.Service
+}
+
+func NewExerciseService(s *flashcards.Service) ExerciseService {
+	return ExerciseService{s: s}
+}
+
+func (e ExerciseService) MakeMatching() Matching {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (e ExerciseService) MakeSentence() Sentence {
+	//TODO implement me
+	panic("implement me")
 }
