eecce6e75f53ec2b65f4dc67d8c34e04926d3855
pelageech <va.oleg2015@yandex.ru>
2023-11-09 17:38:21 +0700

    api models

--- internal/flashcards/models/api.go
+++ internal/flashcards/models/api.go
@@ -7,3 +7,42 @@ type LoadDecksRequest struct {
 type LoadDecksResponse struct {
 	Decks []DeckConfig `json:"decks"`
 }
+
+type GetFlashcardsByDeckIdRequest struct {
+	DeckId DeckId `json:"deck_id"`
+}
+
+type GetFlashcardsByDeckIdResponse struct {
+	Flashcards []Flashcard `json:"flashcards"`
+}
+
+type AddFlashcardToDeckRequest struct {
+	UserId    UserId    `json:"user_id"`
+	DeckId    DeckId    `json:"deck_id"`
+	Flashcard Flashcard `json:"flashcard"`
+}
+
+type AddFlashcardToDeckResponse struct{}
+
+type DeleteFlashcardFromDeckRequest struct {
+	UserId      UserId      `json:"user_id"`
+	FlashcardId FlashcardId `json:"flashcard_id"`
+	DeckId      DeckId      `json:"deck_id"`
+}
+
+type DeleteFlashcardFromDeckResponse struct{}
+
+type NewDeckWithFlashcardsRequest struct {
+	UserId     UserId      `json:"user_id"`
+	DeckConfig DeckConfig  `json:"deck_config"`
+	Flashcards []Flashcard `json:"flashcards"`
+}
+
+type NewDeckWithFlashcardsResponse struct{}
+
+type DeleteDeckRequest struct {
+	UserId UserId `json:"user_id"`
+	DeckId DeckId `json:"deck_id"`
+}
+
+type DeleteDeckResponse struct{}
