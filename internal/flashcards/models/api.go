4a38337260853eb251d4b0ee9a09d4bf7a82cf64
Artyom <91509036+pelageech@users.noreply.github.com>
2023-11-11 14:22:03 +0700

    Handlers for flashcard deck service (#16)
    * added handler of loading decks
    
    * Json io operations and errors refactor (#17)
    
    * created package ioutil.
    ToJSON and FromJSON moved there.
    Created ErrorWriter that should be stored in handlers for error writing
    
    * moving errors
    
    * added logger for error writer
    
    ---------
    
    Co-authored-by: pelageech <va.oleg2015@yandex.ru>
    
    * added handler of loading decks
    
    * api models
    
    * added new handler GetFlashcardsByDeckId
    
    * putting new values returns id (#23)
    
    Co-authored-by: pelageech <va.oleg2015@yandex.ru>
    
    * added handler of loading decks
    
    * api models
    
    * added new handler GetFlashcardsByDeckId
    
    * AddFlashcardToDeck impl
    
    * DeleteFlashcardFromDeck impl
    
    * methods checking
    
    * NewDeckWithFlashcards impl
    
    * DeleteDeck impl
    
    * swagger comments
    
    * swagger comments
    
    * swagger structs
    
    * swagger structs
    
    * swagger comments
    
    * swagger fixed
    
    * swagger fixed 2
    
    * imports
    
    * err declaration removed
    
    * method check removed
    
    ---------
    
    Co-authored-by: pelageech <va.oleg2015@yandex.ru>

---
+++ internal/flashcards/models/api.go
@@ -0,0 +1,175 @@
+package models
+
+// LoadDecksRequest is a struct that defines the request body for the
+// loading deck handler.
+//
+//swagger:model loadDecksRequest
+type LoadDecksRequest struct {
+	// UserId is used for loading for the cards that the user has.
+	//
+	// required: true
+	// example: 189
+	UserId UserId `json:"user_id"`
+}
+
+// LoadDecksResponse is a struct that defines the request body for the
+// loading deck handler.
+type LoadDecksResponse struct {
+	// Decks is a slice of deck with their configs.
+	//
+	// Extensions:
+	// ---
+	// x-property-value: value
+	// x-property-array:
+	//   - value1
+	//   - value2
+	// x-property-array-obj:
+	//   - name: obj
+	//     value: field
+	// ---
+	Decks []DeckConfig `json:"decks"`
+}
+
+// GetFlashcardsByDeckIdRequest is a struct that defines the request body for the
+// loading cards from the deck handler.
+//
+//swagger:model getFlashcardsByDeckIdRequest
+type GetFlashcardsByDeckIdRequest struct {
+	// DeckId defines the deck which flashcards will be loaded.
+	//
+	// required: true
+	// example: 123
+	DeckId DeckId `json:"deck_id"`
+}
+
+// GetFlashcardsByDeckIdResponse is a struct that defines the request body for the
+// loading cards from the deck handler.
+type GetFlashcardsByDeckIdResponse struct {
+	// Flashcards is a slice of flashcards that is returned to the client.
+	//
+	// Extensions:
+	// ---
+	// x-property-value: value
+	// x-property-array:
+	//   - value1
+	//   - value2
+	// x-property-array-obj:
+	//   - name: obj
+	//     value: field
+	// ---
+	Flashcards []Flashcard `json:"flashcards"`
+}
+
+// AddFlashcardToDeckRequest is a struct that defines the request body for the
+// adding cards to the deck handler.
+//
+//swagger:model addFlashcardToDeckRequest
+type AddFlashcardToDeckRequest struct {
+	// UserId is an ID of the user in a storage.
+	//
+	// required: true
+	// example: 189
+	UserId UserId `json:"user_id"`
+
+	// DeckId is an ID of the deck where the flashcard will be added.
+	//
+	// required: true
+	// example: 123
+	DeckId DeckId `json:"deck_id"`
+
+	// Word is a head side of the card.
+	//
+	// required: true
+	// example: Aboba
+	Word Word `json:"word"`
+
+	// Backside is a back side of the card.
+	//
+	// required: true
+	// example: Backside{Type: TypeDefinition, Value: "President"}
+	Backside Backside `json:"backside"`
+}
+
+// AddFlashcardToDeckResponse is a struct that defines the request body for the
+// adding cards to the deck handler.
+type AddFlashcardToDeckResponse struct{}
+
+// DeleteFlashcardFromDeckRequest is a struct that defines the request body for the
+// deleting cards from the deck handler.
+//
+//swagger:model deleteFlashcardFromDeckRequest
+type DeleteFlashcardFromDeckRequest struct {
+	// UserId is an ID of the user.
+	//
+	// required: true
+	// example: 189
+	UserId UserId `json:"user_id"`
+
+	// FlashcardId is an ID of the flashcard.
+	//
+	// required: true
+	// example: 666
+	FlashcardId FlashcardId `json:"flashcard_id"`
+}
+
+// DeleteFlashcardFromDeckResponse is a struct that defines the request body for the
+// deleting cards from the deck handler.
+type DeleteFlashcardFromDeckResponse struct{}
+
+// NewDeckWithFlashcardsRequest is a struct that defines the request body for the
+// creating a new deck handler.
+//
+//swagger:model newDeckWithFlashcardsRequest
+type NewDeckWithFlashcardsRequest struct {
+	// UserId is an ID of the user in a storage.
+	//
+	// required: true
+	// example: 189
+	UserId UserId `json:"user_id"`
+
+	// Name is a name of the deck
+	//
+	// required: true
+	// example: Aboba123
+	Name string `json:"name"`
+
+	// Flashcards is a slice of flashcards that will be added.
+	//
+	// Extensions:
+	// ---
+	// x-property-value: value
+	// x-property-array:
+	//   - value1
+	//   - value2
+	// x-property-array-obj:
+	//   - name: obj
+	//     value: field
+	// ---
+	Flashcards []Flashcard `json:"flashcards"`
+}
+
+// NewDeckWithFlashcardsResponse is a struct that defines the request body for the
+// creating a new deck handler.
+type NewDeckWithFlashcardsResponse struct{}
+
+// DeleteDeckRequest is a struct that defines the request body for the
+// deleting the deck handler.
+//
+//swagger:model deleteDeckRequest
+type DeleteDeckRequest struct {
+	// UserId is an ID of the user in a storage.
+	//
+	// required: true
+	// example: 189
+	UserId UserId `json:"user_id"`
+
+	// DeckId is an ID of the deck where the flashcard will be added.
+	//
+	// required: true
+	// example: 123
+	DeckId DeckId `json:"deck_id"`
+}
+
+// DeleteDeckResponse is a struct that defines the request body for the
+// deleting the deck handler.
+type DeleteDeckResponse struct{}
