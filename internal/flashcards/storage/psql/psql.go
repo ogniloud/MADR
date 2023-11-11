03c5bd1b5d0de3c0e4c253a69e823be284c139dd
pelageech <va.oleg2015@yandex.ru>
2023-11-11 16:24:53 +0700

    psql storage implementation: 1 function

---
+++ internal/flashcards/storage/psql/psql.go
@@ -0,0 +1,91 @@
+package psql
+
+import (
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/ogniloud/madr/internal/db"
+	"github.com/ogniloud/madr/internal/flashcards/models"
+)
+
+type DeckStorage struct {
+	Conn *db.PSQLDatabase
+}
+
+func (d *DeckStorage) GetDecksByUserId(id models.UserId) (models.Decks, error) {
+	rows, err := d.Conn.Query(context.Background(), `SELECT * FROM deck_config WHERE user_id=$1`, id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	decks := models.Decks{}
+
+	cfg := models.DeckConfig{}
+	_, err = pgx.ForEachRow(rows, []any{&cfg.DeckId, &cfg.UserId, &cfg.Name}, func() error {
+		log.Println(cfg)
+		cfg := cfg
+		decks[cfg.DeckId] = cfg
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return decks, nil
+}
+
+func (d *DeckStorage) GetFlashcardsIdByDeckId(id models.DeckId) ([]models.FlashcardId, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) GetFlashcardById(id models.FlashcardId) (models.Flashcard, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) GetLeitnerByUserIdCardId(id models.UserId, flashcardId models.FlashcardId) (models.UserLeitner, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) GetUserInfo(uid models.UserId) (models.UserInfo, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) PutAllFlashcards(id models.DeckId, cards []models.Flashcard) ([]models.FlashcardId, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) PutNewDeck(config models.DeckConfig) (models.DeckId, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) PutAllUserLeitner(uls []models.UserLeitner) ([]models.LeitnerId, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) DeleteFlashcardFromDeck(cardId models.FlashcardId) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) DeleteUserDeck(userId models.UserId, id models.DeckId) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (d *DeckStorage) UpdateLeitner(ul models.UserLeitner) error {
+	//TODO implement me
+	panic("implement me")
+}
