efe2a03de203dce0010e61d881163423b2b29911
pelageech <va.oleg2015@yandex.ru>
2023-11-11 17:53:31 +0700

    psql_test

---
+++ internal/flashcards/storage/psql/psql_test.go
@@ -0,0 +1,47 @@
+package psql
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/charmbracelet/log"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/ogniloud/madr/internal/db"
+	"github.com/ogniloud/madr/internal/flashcards/models"
+)
+
+type PSQLSuite struct {
+	suite.Suite
+	repo *DeckStorage
+}
+
+const connStringTest = "user=postgres password=postgres host=localhost port=5432 dbname=postgres"
+
+func (s *PSQLSuite) SetupSuite() {
+	conn, err := db.NewPSQLDatabase(context.Background(), connStringTest, log.New(io.Discard))
+	if err != nil {
+		s.T().Fatal(err)
+	}
+
+	s.repo = &DeckStorage{Conn: conn}
+}
+
+func (s *PSQLSuite) TearDownSuite() {
+	_ = s.repo.Conn.Close(context.Background())
+}
+
+func (s *PSQLSuite) TestDeckStorage_GetDecksByUserId() {
+	decks, err := s.repo.GetDecksByUserId(1)
+
+	if assert.NoError(s.T(), err) {
+		assert.Equal(s.T(), 2, len(decks))
+		assert.Equal(s.T(), decks.Keys(), []models.DeckId{12, 23})
+	}
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(PSQLSuite))
+}
