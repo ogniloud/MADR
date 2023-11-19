2ac3b16db1759b715182abdef636d3bed87651ac
Dmitriy Krasnov <43964259+peyuaa@users.noreply.github.com>
2023-11-19 22:48:19 +0700

    Added postgresql as database (Docker init + signin + signup requests to psql) (#33)
    * Added postgresql as database
    
    * update go version
    
    ---------
    
    Co-authored-by: Dmitriy Krasnov <dekrasnov@avito.ru>

--- internal/data/dl.go
+++ internal/data/dl.go
@@ -2,73 +2,155 @@ package data
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/ogniloud/madr/internal/database"
 	"github.com/ogniloud/madr/internal/models"
 )
 
-// ErrEmailExists is an error returned when a user with the given email already exists.
-var ErrEmailExists = fmt.Errorf("user with this email already exists")
+// ErrEmailOrUsernameExists is an error returned when a user with the given email already exists.
+var ErrEmailOrUsernameExists = fmt.Errorf("user with this email or username already exists")
 
 // Datalayer is a struct that helps us to interact with the data.
 type Datalayer struct {
-	// users is a slice of models.User
-	// This is our in-memory database. We will replace this with a real database later.
-	users models.Users
+	// db is a database.
+	db *database.Database
+
+	// saltLength is the length of the salt.
+	// We will use this to generate a salt for the password.
+	saltLength int
+
+	// tokenExpirationTime is the time after which the token will expire.
+	tokenExpirationTime time.Duration
+
+	// signKey is a key to sign the token.
+	signKey []byte
 }
 
 // New returns a new Datalayer struct.
-func New() *Datalayer {
-	return &Datalayer{}
+func New(db *database.Database, saltLength int, tokenExpirationTime time.Duration, signKey []byte) *Datalayer {
+	return &Datalayer{
+		db:                  db,
+		saltLength:          saltLength,
+		tokenExpirationTime: tokenExpirationTime,
+		signKey:             signKey,
+	}
 }
 
-// isEmailExists is a helper function to check if a user with the given email already exists.
+// isEmailOrUsernameExists is a helper function to check if a user with the given email already exists.
 // Returns true if the user exists, false otherwise.
-func (d *Datalayer) isEmailExists(email string) bool {
-	for _, user := range d.users {
-		if user.Email == email {
-			return true
-		}
+func (d *Datalayer) isEmailOrUsernameExists(ctx context.Context, username, email string) (bool, error) {
+	has, err := d.db.HasEmailOrUsername(ctx, username, email)
+	if err != nil {
+		return false, fmt.Errorf("unable to check if email exists in isEmailOrUsernameExists: %w", err)
 	}
 
-	return false
+	return has, nil
 }
 
 // isPasswordCorrect is a helper function to check if the given password is correct for the given email.
-func (d *Datalayer) isPasswordCorrect(email, password string) bool {
-	for _, user := range d.users {
-		if user.Email == email && user.Password == password {
-			return true
-		}
+func (d *Datalayer) isPasswordCorrect(ctx context.Context, username, password string) (bool, error) {
+	salt, hash, err := d.db.GetSaltAndHash(ctx, username)
+	if err != nil {
+		return false, fmt.Errorf("unable to get salt and hash in isPasswordCorrect: %w", err)
 	}
 
-	return false
+	// Compare the password with the hash
+	err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(password+salt))
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (d *Datalayer) generateSalt() (string, error) {
+	saltBytes := make([]byte, d.saltLength)
+
+	_, err := rand.Read(saltBytes)
+	if err != nil {
+		return "", err
+	}
+
+	salt := base64.URLEncoding.EncodeToString(saltBytes)
+	return salt, nil
+}
+
+// generateToken is a helper function to generate a JWT token.
+func (d *Datalayer) generateToken(username string) (string, error) {
+	// Set the expiration time of the token
+	expirationTime := time.Now().Add(d.tokenExpirationTime * time.Minute)
+
+	// Create the JWT claims, which includes the username and expiry time
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"username": username,
+		"exp":      expirationTime.Unix(),
+	})
+
+	// Sign the token with our secret key
+	signedToken, err := token.SignedString(d.signKey)
+	if err != nil {
+		return "", fmt.Errorf("unable to sign token in generateToken: %w", err)
+	}
+
+	return signedToken, nil
 }
 
 // CreateUser is a function to create a new user.
-func (d *Datalayer) CreateUser(_ context.Context, user models.User) error {
-	if d.isEmailExists(user.Email) {
-		return ErrEmailExists
+func (d *Datalayer) CreateUser(ctx context.Context, user models.User) error {
+	// Check if user with this email already exists
+	has, err := d.isEmailOrUsernameExists(ctx, user.Username, user.Email)
+	if err != nil {
+		return fmt.Errorf("unable to check if email or username exists in CreateUser: %w", err)
 	}
 
-	if len(d.users) == 0 {
-		user.ID = 1
-	} else {
-		user.ID = d.users[len(d.users)-1].ID + 1
+	// If user with this email or username already exists, return an error
+	if has {
+		return ErrEmailOrUsernameExists
 	}
 
-	d.users = append(d.users, user)
+	// Generate salt
+	salt, err := d.generateSalt()
+	if err != nil {
+		return fmt.Errorf("unable to generate salt in CreateUser: %w", err)
+	}
+
+	// Hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password+salt), bcrypt.DefaultCost)
+	if err != nil {
+		return fmt.Errorf("unable to hash password in CreateUser: %w", err)
+	}
+
+	err = d.db.InsertUser(ctx, user.Username, salt, string(hash), user.Email)
+	if err != nil {
+		return fmt.Errorf("unable to insert user in CreateUser: %w", err)
+	}
 
 	return nil
 }
 
 // SignInUser is a function to sign in a user.
 // It returns an authorization token if the user is signed in successfully.
-func (d *Datalayer) SignInUser(_ context.Context, email, password string) (authorization string, err error) {
-	if d.isPasswordCorrect(email, password) {
-		authorization = "Bearer blablablaIMATOKENyouAREpoorBASTARD"
-		return
+func (d *Datalayer) SignInUser(ctx context.Context, username, password string) (string, error) {
+	correct, err := d.isPasswordCorrect(ctx, username, password)
+	if err != nil {
+		return "", fmt.Errorf("unable to check if password is correct in SignInUser: %w", err)
 	}
 
-	return authorization, models.ErrUnauthorized
+	if !correct {
+		return "", models.ErrUnauthorized
+	}
+
+	token, err := d.generateToken(username)
+	if err != nil {
+		return "", fmt.Errorf("unable to generate token in SignInUser: %w", err)
+	}
+
+	return "Bearer " + token, nil
 }
--- internal/models/data.go
+++ internal/models/data.go
@@ -3,6 +3,7 @@ package models
 // User is a struct that defines the user model.
 type User struct {
 	ID       int    `json:"id"`
+	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
