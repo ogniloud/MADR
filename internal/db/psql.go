e04d81b249d7a42fc619c4a5301de293b840fc13
pelageech <va.oleg2015@yandex.ru>
2023-12-15 22:00:08 +0700

    psql

--- internal/db/psql.go
+++ internal/db/psql.go
@@ -4,24 +4,24 @@ import (
 	"context"
 
 	"github.com/charmbracelet/log"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // PSQLDatabase accesses to PostgresSQL with other fields.
 type PSQLDatabase struct {
-	*pgx.Conn
+	*pgxpool.Pool
 	logger *log.Logger
 }
 
 // NewPSQLDatabase creates a new connection to db and tries to connect to it.
 func NewPSQLDatabase(ctx context.Context, connString string, logger *log.Logger) (*PSQLDatabase, error) {
-	conn, err := pgx.Connect(ctx, connString)
+	conn, err := pgxpool.New(ctx, connString)
 	if err != nil {
 		return nil, err
 	}
 
 	return &PSQLDatabase{
-		Conn:   conn,
+		Pool:   conn,
 		logger: logger,
 	}, nil
 }
