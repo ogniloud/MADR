536c8fe791e4d9fbc836b453803cca27f0cd52e2
Artyom Blaginin <91509036+pelageech@users.noreply.github.com>
2024-02-10 15:21:53 +0700

    Storage refactor (#54)
    * refactored interface for flashcard storage
    
    * added todos
    
    ---------
    
    Co-authored-by: pelageech <va.oleg2015@yandex.ru>

--- internal/flashcards/storage/storage.go
+++ internal/flashcards/storage/storage.go
@@ -10,8 +10,17 @@ import (
 //
 //go:generate go run github.com/vektra/mockery/v2@v2.36.0 --name Storage
 type Storage interface {
-	// GetDecksByUserId returns all decks the user has.
-	GetDecksByUserId(ctx context.Context, id models.UserId) (models.Decks, error)
+	FlashcardCRUD
+	DeckCRUD
+	LeitnerCRUD
+
+	// GetUserInfo returns models.UserInfo by models.UserId.
+	GetUserInfo(ctx context.Context, uid models.UserId) (models.UserInfo, error)
+}
+
+type FlashcardCRUD interface {
+	// PutAllFlashcards appends flashcards to the deck by appending new rows in models.Flashcard storage.
+	PutAllFlashcards(ctx context.Context, id models.DeckId, cards []models.Flashcard) ([]models.FlashcardId, error)
 
 	// GetFlashcardsIdByDeckId returns the id of the flashcards in the deck.
 	GetFlashcardsIdByDeckId(ctx context.Context, id models.DeckId) ([]models.FlashcardId, error)
@@ -19,28 +28,35 @@ type Storage interface {
 	// GetFlashcardById returns models.Flashcard by its id.
 	GetFlashcardById(ctx context.Context, id models.FlashcardId) (models.Flashcard, error)
 
-	// GetLeitnerByUserIdCardId returns models.UserLeitner by user and card.
-	GetLeitnerByUserIdCardId(ctx context.Context, id models.UserId, flashcardId models.FlashcardId) (models.UserLeitner, error)
+	//todo: update
 
-	// GetUserInfo returns models.UserInfo by models.UserId.
-	GetUserInfo(ctx context.Context, uid models.UserId) (models.UserInfo, error)
+	// DeleteFlashcardFromDeck deletes a record from models.Flashcard storage about flashcard.
+	DeleteFlashcardFromDeck(ctx context.Context, cardId models.FlashcardId) error
+}
 
-	// PutAllFlashcards appends flashcards to the deck by appending new rows in models.Flashcard storage.
-	PutAllFlashcards(ctx context.Context, id models.DeckId, cards []models.Flashcard) ([]models.FlashcardId, error)
-
+type DeckCRUD interface {
 	// PutNewDeck appends a new deck configuration.
 	PutNewDeck(ctx context.Context, config models.DeckConfig) (models.DeckId, error)
 
+	// GetDecksByUserId returns all decks the user has.
+	GetDecksByUserId(ctx context.Context, id models.UserId) (models.Decks, error)
+
+	//todo: update
+
+	// DeleteUserDeck removes the entry from models.DeckConfig that user with userId has a deck.
+	DeleteUserDeck(ctx context.Context, userId models.UserId, id models.DeckId) error
+}
+
+type LeitnerCRUD interface {
 	// PutAllUserLeitner appends all the models.UserLeitner in storage.
 	// Returns a slice of ids in order of the slice of models.UserLeitner.
 	PutAllUserLeitner(ctx context.Context, uls []models.UserLeitner) ([]models.LeitnerId, error)
 
-	// DeleteFlashcardFromDeck deletes a record from models.Flashcard storage about flashcard.
-	DeleteFlashcardFromDeck(ctx context.Context, cardId models.FlashcardId) error
-
-	// DeleteUserDeck removes the entry from models.DeckConfig that user with userId has a deck.
-	DeleteUserDeck(ctx context.Context, userId models.UserId, id models.DeckId) error
+	// GetLeitnerByUserIdCardId returns models.UserLeitner by user and card.
+	GetLeitnerByUserIdCardId(ctx context.Context, id models.UserId, flashcardId models.FlashcardId) (models.UserLeitner, error)
 
 	// UpdateLeitner updates a record in the database when the models.LeitnerId matches.
 	UpdateLeitner(ctx context.Context, ul models.UserLeitner) error
+
+	//todo: delete
 }
