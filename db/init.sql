9a72cd70760ba882967db97329e176d3384447bf
Artyom Blaginin <91509036+pelageech@users.noreply.github.com>
2024-02-11 15:27:49 +0700

    added commands to create group tables (#48)
    * added commands to create group tables
    
    * added timestamps for feed
    
    * описание поведения группы и колод
    
    * DB Tables: Add table for followers
    
    * DB Tables: Add table for invites into groups
    
    * DB Tables: Fix constraints
    
    * fixed sql
    
    ---------
    
    Co-authored-by: pelageech <va.oleg2015@yandex.ru>
    Co-authored-by: MIZINCHIK <mizinchik@yahoo.com>

--- db/init.sql
+++ db/init.sql
@@ -35,4 +35,116 @@ CREATE TABLE IF NOT EXISTS user_leitner (
     cool_down TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user_credentials(user_id),
     FOREIGN KEY (card_id) REFERENCES flashcard(card_id)
-)
+);
+
+-- Groups
+
+CREATE TABLE IF NOT EXISTS groups (
+    group_id SERIAL PRIMARY KEY,
+    creator_id SERIAL,
+    name VARCHAR(50) NOT NULL,
+    time_created TIMESTAMP,
+
+    FOREIGN KEY (creator_id) REFERENCES user_credentials(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS group_decks (
+    group_id SERIAL,
+    deck_id SERIAL, -- колода, которую видят все члены группы
+    time_shared TIMESTAMP, -- время шаринга колодой
+    UNIQUE(group_id, deck_id),
+
+    FOREIGN KEY (group_id) REFERENCES groups(group_id),
+    FOREIGN KEY (deck_id) REFERENCES deck_config(deck_id)
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+    group_id SERIAL,
+    user_id SERIAL, -- юзер является членом группы group_id
+    time_joined TIMESTAMP,
+    UNIQUE(group_id, user_id),
+
+    FOREIGN KEY (group_id) REFERENCES groups(group_id),
+    FOREIGN KEY (user_id) REFERENCES user_credentials(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS group_invites (
+    group_id SERIAL,
+    user_id SERIAL, -- юзер получил инвайт группы, но ещё его не принял
+    time_sent TIMESTAMP,
+    UNIQUE(group_id, user_id),
+
+    FOREIGN KEY (group_id) REFERENCES groups(group_id),
+    FOREIGN KEY (user_id) REFERENCES user_credentials(user_id)
+);
+
+--
+CREATE TABLE IF NOT EXISTS links (
+    deck_id SERIAL PRIMARY KEY, -- дека откопирована от copied_from
+    copied_from SERIAL,
+    updatable BOOLEAN, -- можно ли обновлять колоду
+
+    FOREIGN KEY (deck_id) REFERENCES deck_config(deck_id),
+    FOREIGN KEY (copied_from) REFERENCES deck_config(deck_id),
+    CHECK ( deck_id != links.copied_from )
+);
+
+CREATE TABLE IF NOT EXISTS group_shared (
+    deck_id SERIAL PRIMARY KEY,
+    time_shared TIMESTAMP,
+
+    FOREIGN KEY (deck_id) REFERENCES deck_config(deck_id)
+);
+
+CREATE TABLE IF NOT EXISTS public_shared (
+    deck_id SERIAL PRIMARY KEY,
+    time_shared TIMESTAMP,
+
+    FOREIGN KEY (deck_id) REFERENCES deck_config(deck_id)
+);
+
+CREATE TABLE IF NOT EXISTS followers (
+    user_id SERIAL,
+    follower_id SERIAL, -- фолловер отслеживает действия юзера в фиде
+    time_followed TIMESTAMP,
+    UNIQUE(user_id, follower_id),
+
+    FOREIGN KEY (user_id) REFERENCES user_credentials(user_id),
+    FOREIGN KEY (follower_id) REFERENCES user_credentials(user_id)
+);
+
+-- Сценарии колод
+--
+-- 1. Создание
+--      При создании колоды человеком user_id в транзакции создаётся запись в deck_config
+--      (автоинкремент deck_id, user_id),
+--      а карточки помещаются во flashcards, для каждой карточки новая строка, причём поле
+--      deck_id у всех одинаковое - id новой колоды.
+--
+-- 2. Шаринг подписавшимся [на меня] или участникам в группе
+--      Если юзер делится своей колодой, она добавляется в одну из таблиц.
+--          group_shared    |deck_id, timestamp|    (только пользователи некоторой группы, в которой
+--          ВЫ ЯВЛЯЕТЕСЬ СОЗДАТЕЛЕМ могут видеть ваши колоды)
+--          public_shared   |deck_id, timestamp|    (все пользователи видят в фид вашу колоду и могут добавить её к себе)
+--
+-- 3. Копирование колоды из фида
+--  При нажатии на кнопку "копировать" (например, колоду с id=2), сначала проверяется,
+--  нет ли у юзера колоды, которая уже откопирована от колоды с id=2. Используется таблица links.
+--  Для юзера в deck_config создается новая колода, получаем deck_id.
+--  Затем в эту таблицу заносится строка (deck_id, copied_from=2, updatable=true). Таблица
+--  flashcards заполняется новыми картами, эквивалентные из старой колоды, но deck_id будет новый.
+--  Таким образом, мы сделали deep copy колоды.
+--
+-- 4. Копирование колоды из группы
+--  Когда создатель шарит колоду группе, люди автоматически её заимствуют себе, но без deep copy
+--  всей колоды.
+--  По аналогии выше создаётся запись в deck_config, но в deck_config заносится строка
+--  (deck_id, copied_from=2, updatable=false), а таблица flashcards не трогается.
+--
+-- 5. Изменение колоды
+--  Изменять можно только свои колоды или скопированные из фида.
+--
+-- 6. Удаление колоды.
+--  Удалить можно только свои колоды или скопированные из фида.
+--  Колоды групповые удалять нельзя (если не создатель).
+--  Если человек, удаляющий колоду, создатель... ЗАВЕРШИТЬ.
