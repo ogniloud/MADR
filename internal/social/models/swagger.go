c3af6e3a8f11d23c04b5421598a66db752a53ee3
Dmitriy Krasnov <dekrasnov@avito.ru>
2024-02-25 12:48:50 +0700

    added Unfollow swagger

--- internal/social/models/swagger.go
+++ internal/social/models/swagger.go
@@ -84,3 +84,28 @@ type SwaggerFollowInternalServerErrorResponse struct {
 	// in: body
 	Body ioutil.GenericError
 }
+
+// swagger:model unfollowRequest
+// SwaggerUnfollowRequest is a struct that defines the request body for the Unfollow endpoint.
+type SwaggerUnfollowRequest struct {
+	// in: body
+	Body UnfollowRequest
+}
+
+// swagger:response unfollowNoContentResponse
+// SwaggerUnfollowNoContentResponse is a struct that defines the response body for the Unfollow endpoint.
+type SwaggerUnfollowNoContentResponse struct{}
+
+// swagger:response unfollowBadRequestResponse
+// SwaggerUnfollowBadRequestResponse is a struct that defines the response body for the Unfollow endpoint.
+type SwaggerUnfollowBadRequestResponse struct {
+	// in: body
+	Body ioutil.GenericError
+}
+
+// swagger:response unfollowInternalServerErrorResponse
+// SwaggerUnfollowInternalServerErrorResponse is a struct that defines the response body for the Unfollow endpoint.
+type SwaggerUnfollowInternalServerErrorResponse struct {
+	// in: body
+	Body ioutil.GenericError
+}
