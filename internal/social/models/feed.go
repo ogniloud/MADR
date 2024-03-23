33c44f080815741d6be49e17133b723d4dab0a60
pelageech <va.oleg2015@yandex.ru>
2024-03-23 20:35:36 +0700

    new post

--- internal/social/models/feed.go
+++ internal/social/models/feed.go
@@ -1,83 +1,20 @@
 package models
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
-type FeedPostType int
+type FeedPostType string
 
 const (
-	Invite = FeedPostType(iota)
-	SharedFromGroup
-	SharedFromFollowing
-	FollowingSubscribed
+	Invite              = "invite_data"
+	SharedFromGroup     = "shared_from_group_data"
+	SharedFromFollowing = "shared_from_following_data"
+	FollowingSubscribed = "following_subscribed_data"
 )
 
-type FeedData interface {
-	Type() FeedPostType
-}
-
 type Post struct {
-	// Type is a number for type of the data. Use Data.Type()
-	// to set correct number to the field.
-	Type FeedPostType `json:"type"`
-	Data FeedData     `json:"data"`
-}
-
-func (p *Post) UnmarshalJSON(b []byte) error {
-	m := map[string]any{}
-	if err := json.Unmarshal(b, &m); err != nil {
-		return err
-	}
-
-	temp, ok := m["type"]
-	if !ok {
-		return &json.UnmarshalTypeError{Field: "type"}
-	}
-
-	t, ok := temp.(float64)
-	if !ok {
-		return &json.UnmarshalTypeError{Value: fmt.Sprint(temp), Field: "type"}
-	}
-
-	data, ok := m["data"]
-	if !ok {
-		return &json.UnmarshalTypeError{Field: "data"}
-	}
-
-	bb, _ := json.Marshal(data)
-
-	p.Type = FeedPostType(t)
-
-	switch p.Type {
-	case Invite:
-		data := InviteData{}
-		if err := json.Unmarshal(bb, &data); err != nil {
-			return err
-		}
-		p.Data = data
-	case SharedFromGroup:
-		data := SharedFromGroupData{}
-		if err := json.Unmarshal(bb, &data); err != nil {
-			return err
-		}
-		p.Data = data
-	case SharedFromFollowing:
-		data := SharedFromFollowingData{}
-		if err := json.Unmarshal(bb, &data); err != nil {
-			return err
-		}
-		p.Data = data
-	case FollowingSubscribed:
-		data := FollowingSubscribedData{}
-		if err := json.Unmarshal(bb, &data); err != nil {
-			return err
-		}
-		p.Data = data
-	}
-
-	return nil
+	Type                    FeedPostType             `json:"type"`
+	InviteData              *InviteData              `json:"invite_data"`
+	SharedFromGroupData     *SharedFromGroupData     `json:"shared_from_group_data"`
+	SharedFromFollowingData *SharedFromFollowingData `json:"shared_from_following_data"`
+	FollowingSubscribedData *FollowingSubscribedData `json:"following_subscribed_data"`
 }
 
 type InviteData struct {
@@ -87,19 +24,11 @@ type InviteData struct {
 	GroupName   string  `json:"group_name"`
 }
 
-func (d InviteData) Type() FeedPostType {
-	return Invite
-}
-
 type SharedFromGroupData struct {
 	GroupId   GroupId `json:"group_id"`
 	GroupName string  `json:"group_name"`
 	DeckId    DeckId  `json:"deck_id"`
 	DeckName  string  `json:"deck_name"`
-}
-
-func (d SharedFromGroupData) Type() FeedPostType {
-	return SharedFromGroup
 }
 
 type SharedFromFollowingData struct {
@@ -109,17 +38,9 @@ type SharedFromFollowingData struct {
 	DeckName   string `json:"deck_name"`
 }
 
-func (d SharedFromFollowingData) Type() FeedPostType {
-	return SharedFromFollowing
-}
-
 type FollowingSubscribedData struct {
 	FollowerId   UserId `json:"follower_id"`
 	FollowerName string `json:"follower_name"`
 	AuthorId     UserId `json:"author_id"`
 	AuthorName   string `json:"author_name"`
-}
-
-func (d FollowingSubscribedData) Type() FeedPostType {
-	return FollowingSubscribed
 }
