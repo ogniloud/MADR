017a13b864d44c00ad67b015e8d8c15151bad9df
Artem Blaginin <artblaginin@yandex-team.ru>
2024-04-29 19:35:19 +0500

    added a new column

--- db/init.sql
+++ db/init.sql
@@ -21,6 +21,9 @@ CREATE TABLE IF NOT EXISTS flashcard (
     answer VARCHAR(100) NOT NULL
 );
 
+ALTER TABLE IF EXISTS flashcard
+    ADD IF NOT EXISTS multiple_backside json NULL;
+
 CREATE TABLE IF NOT EXISTS user_info (
     user_id SERIAL PRIMARY KEY,
     max_box INT NOT NULL CHECK (max_box > 0),
