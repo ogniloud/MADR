366ed215376cbe32846f2279ab9c1298a3475469
pelageech <va.oleg2015@yandex.ru>
2024-05-12 18:11:58 +0700

    mockery

--- internal/flashcards/storage/mocks/Storage.go
+++ internal/flashcards/storage/mocks/Storage.go
@@ -170,6 +170,32 @@ func (_m *Storage) GetLeitnerByUserIdCardId(ctx context.Context, id int, flashca
 	return r0, r1
 }
 
+// GetRandomCardDeckN provides a mock function with given fields: ctx, userId, deckId, down, n
+func (_m *Storage) GetRandomCardDeckN(ctx context.Context, userId int, deckId int, down models.CoolDown, n int) ([]int, error) {
+	ret := _m.Called(ctx, userId, deckId, down, n)
+
+	var r0 []int
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context, int, int, models.CoolDown, int) ([]int, error)); ok {
+		return rf(ctx, userId, deckId, down, n)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context, int, int, models.CoolDown, int) []int); ok {
+		r0 = rf(ctx, userId, deckId, down, n)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]int)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context, int, int, models.CoolDown, int) error); ok {
+		r1 = rf(ctx, userId, deckId, down, n)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // GetUserInfo provides a mock function with given fields: ctx, uid
 func (_m *Storage) GetUserInfo(ctx context.Context, uid int) (models.UserInfo, error) {
 	ret := _m.Called(ctx, uid)
